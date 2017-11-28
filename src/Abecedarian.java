12e250e93d64a611ebb47435851848800ebcad6c
Eli Heuer <eliheuer@gmail.com>
2017-11-28 18:39:57 -0500

    Refactoring

--- src/Abecedarian.java
+++ src/Abecedarian.java
@@ -1,37 +1,63 @@
+// Exercise 4: Eli Heuer, CMP167 Project 2
+
 import java.util.*;
 
-/** Enter an integer and prints all its divisors. */
+/**
+ *
+ * A word is said to be “abecedarian” if the letters in the word appear 
+ * in alphabetical order. For example, the following are all six-letter 
+ * English abecedarian words: 
+ * 
+ * abdest, acknow, acorsy, adempt, adipsy, agnosy, befist, behint, beknow, 
+ * bijoux, biopsy, cestuy, chintz, deflux, dehors, dehort, deinos, diluvy, 
+ *
+ * Write a method called isAbecedarian that takes a String and returns a boolean 
+ * indicating whether the word is abecedarian. Your method can be iterative or 
+ * recursive.
+ *
+ *@author Eli Heuer, elih@protonmail.com
+ *
+ */
 public class Abecedarian {
-    
     public static void main(String[] args) {
-	String test = "dehort";
-	
-	// input
-	boolean abTest;
-	abTest = isAbecedarian(test);
-	System.out.println("abTest = "+abTest);
 
+	// Initialize variables:
+	String userText = null;
+
+	// Get user input:
+	Scanner in = new Scanner(System.in);
+        System.out.println("Examples of abecedarians: abdest, acknow");
+	System.out.print("\nEnter phrase: ");
+	userText = in.nextLine();
+
+	// Test input:
+	boolean abTest = isAbecedarian(userText);
+
+        // Print boolean result:
 	if (abTest) {
-	    System.out.println(test+" is an abecedarian.");
+	    System.out.println("\n"+userText+" is an abecedarian.");
 	} else {
-	    System.out.println(test+" is not an abecedarian.");
+	    System.out.println("\n"+userText+" is not an abecedarian.");
 	}
     }
     
+    // Returns true is word is an abecedarian:
     public static boolean isAbecedarian (String word) {
-	int length = word.length();
+
+	// Check if word is a usable string:
+        int length = word.length();
 	if (length <= 1) {
 	    return true;
 	}
+        
+        // Initialize variables:
 	char previous;
 	char current;
-	
-	System.out.println("length: "+length);
-	
+
+        // Test is word is abecedarian:
 	for (int i = 1; i < length; ++i) {
 	    previous = word.charAt(i - 1);
 	    current = word.charAt(i);
-	    System.out.println(previous);
 	    if (previous > current) {
 		return false;
 	    }
--- src/Recurse.java
+++ src/Recurse.java
@@ -1,9 +1,67 @@
+// Exercise 3: Eli Heuer, CMP167 Project 2
+
 import java.util.*;
 
-/** Enter an integer and prints all its divisors. */
-public class DivisorList {
+/**
+ *
+ *
+ *1) Using these methods, and without using any other String methods, 
+ *   write a method called printString that takes a string as a parameter 
+ *   and that displays the letters of the string, one on each line. It 
+ *   should be a void method.
+ *
+ *2) Again using only these methods, write a method called printBackward 
+ *   that does the same thing as printString but that displays the string 
+ *   backward (again, one character per line).
+ *
+ *3) Now write a method called reverseString that takes a string as a parameter 
+ *   and that returns a new string as a return value. The new string should 
+ *   contain the same letters as the parameter, but in reverse order.
+ *
+ *   String backwards = reverseString("coffee"); 
+ *   System.out.println(backwards);
+ *
+ *   The output of this example code should be: 
+ *   eeffoc
+ *
+ *4) A palindrome is a word that reads the same both forward and backward, 
+ *   like “otto” and “palindromeemordnilap”. Here’s one way to test whether 
+ *   a string is a palindrome:
+ *
+ *   A single letter is a palindrome, a two-letter word is a palindrome if 
+ *   the letters are the same, and any other word is a palindrome if the first 
+ *   letter is the same as the last and the middle is a palindrome.
+ *
+ *   Write a recursive method named isPalindrome that takes a String and returns a 
+ *   boolean indicating whether the word is a palindrome.
+ *
+ *@author Eli Heuer, elih@protonmail.com
+ *
+ */
+
+public class Recurse {
     
     public static void main(String[] args) {
 	// int input
+    }
+
+    /** * Returns the first character of the given String. */ 
+    public static char first(String s) { 
+        return s.charAt(0); 
+    } 
+
+    /** * Returns all but the first letter of the given String. */ 
+    public static String rest(String s) { 
+        return s.substring(1); 
+    }
+
+    /** * Returns all but the first and last letter of the String. */
+    public static String middle(String s) { 
+        return s.substring(1, s.length() - 1); 
+    } 
+
+    /** * Returns the length of the given String. */ 
+    public static int length(String s) {
+        return s.length(); 
     }
 }
